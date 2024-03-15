85a731a8dc3c83015765b9cd84a7f157fabf8376
datle-dev <dle.dev@outlook.com>
2024-03-15 12:51:17 -0400

    Rewrite ctb_queue.py to be a functional module
    Original CTBQueue class resulting from class-based approach appears
    unnecessary for now since the queue (order of party member/enemy turns)
    itself doesn't need attributes. The info that could be attributes for a
    CTBQueue class are just as easily retrieved from whatever is returned
    from a function version and don't need to be stored persistently.
    There is also only ever 1 queue active in a given battle or encounter
    between the party and enemies, so without need for multiple objects
    inheriting the CTBQueue class nor need to track the state, the class
    can be dissolved into a function and the script made a module.

--- ctb_queue.py
+++ ctb_queue.py
@@ -1,49 +1,28 @@
-from dataclasses import dataclass
 from typing import List
 
 from creature import Creature
 
-@dataclass
-class CTBQueue:
-    participants: List[Creature]
-    predicted_queue: List[Creature] = None
-    next_attacker: Creature = None
+def get_queue(participants: List[Creature], num_turns: int = 10) -> List[Creature]:
+    '''Predicts order of next 10 turns'''
+    queue_members = [{'name': x.name, 'tick': x.tick, 'counter': x.counter, 'creature': x} for x in participants]
+    predicted_queue = []
 
+    while len(predicted_queue) < num_turns:
+        # next attacker is one with lowest counter
+        next_attacker = min(queue_members, key=lambda x: x['counter'])
+        next_attacker_index = queue_members.index(next_attacker)
 
-    def __post_init__(self) -> None:
-        self.get_queue()
-        self.get_next_attacker()
+        # reduce all members counter by attacker counter to simulate advancement of time
+        # decrement value needs to be assigned to separate value, otherwise it doesn't work
+        counter_decrement = next_attacker['counter']
+        for member in queue_members:
+            member['counter'] -= counter_decrement
 
+        # add attacker tick * 3 (default attack ability rank) to their counter
+        # increment value needs to be assigned to separate value, otherwise it doesn't work
+        counter_increment = next_attacker['tick'] * 3
+        queue_members[next_attacker_index]['counter'] += counter_increment
 
-    def get_queue(self) -> None:
-        queue_members = [{'name': x.name, 'tick': x.tick, 'counter': x.counter, 'creature': x} for x in self.participants]
-        predicted_queue = []
-
-        while len(predicted_queue) < 10:
-            # next attacker is one with lowest counter
-            next_attacker = min(queue_members, key=lambda x: x['counter'])
-            next_attacker_index = queue_members.index(next_attacker)
-
-            # reduce all members counter by attacker counter to simulate advancement of time
-            # decrement value needs to be assigned to separate value, otherwise it doesn't work
-            counter_decrement = next_attacker['counter']
-            for member in queue_members:
-                member['counter'] -= counter_decrement
-
-            # add attacker tick * 3 (default attack ability rank) to their counter
-            # increment value needs to be assigned to separate value, otherwise it doesn't work
-            counter_increment = next_attacker['tick'] * 3
-            queue_members[next_attacker_index]['counter'] += counter_increment
-
-            predicted_queue.append(next_attacker['creature'])
-        
-        self.predicted_queue = predicted_queue
-
-
-    def get_next_attacker(self) -> None:
-        self.next_attacker = self.predicted_queue[0]
-
-
-    def __str__(self) -> str:
-        for ind, x in enumerate(self.predicted_queue):
-            print(f'{ind}: {x.name}')
+        predicted_queue.append(next_attacker['creature'])
+    
+    return predicted_queue
