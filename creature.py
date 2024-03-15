2d77fc92c43039b8cc96059b5478fc9a87f552af
datle-dev <dle.dev@outlook.com>
2024-03-15 10:29:02 -0400

    Generalize function for any creature, not just party members

--- creature.py
+++ creature.py
@@ -21,24 +21,24 @@ class Creature:
     counter: int = field(default=0)
 
 
-def load_party_member(name, json_path) -> Creature:
+def load_creature(name, json_path) -> Creature:
     with open(json_path) as f:
-        main_characters = json.load(f)
+        entities = json.load(f)
 
-    party_member = Creature(
-        name=main_characters[name]['name'],
-        category=main_characters[name]['category'],
-        level=main_characters[name]['level'],
-        hp=main_characters[name]['hp'],
-        hp_max=main_characters[name]['hp_max'],
-        strength=main_characters[name]['strength'],
-        defense=main_characters[name]['defense'],
-        magic=main_characters[name]['magic'],
-        magic_defense=main_characters[name]['magic_defense'],
-        agility=main_characters[name]['agility'],
-        accuracy=main_characters[name]['accuracy'],
-        evasion=main_characters[name]['evasion'],
-        luck=main_characters[name]['luck'],
+    entity = Creature(
+        name=entities[name]['name'],
+        category=entities[name]['category'],
+        level=entities[name]['level'],
+        hp=entities[name]['hp'],
+        hp_max=entities[name]['hp_max'],
+        strength=entities[name]['strength'],
+        defense=entities[name]['defense'],
+        magic=entities[name]['magic'],
+        magic_defense=entities[name]['magic_defense'],
+        agility=entities[name]['agility'],
+        accuracy=entities[name]['accuracy'],
+        evasion=entities[name]['evasion'],
+        luck=entities[name]['luck'],
     )
 
-    return party_member
+    return entity
