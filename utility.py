cf8c8dc67c0bfd1f3ae2748fc3d879c7010efd76
datle-dev <dle.dev@outlook.com>
2024-03-14 21:31:06 -0400

    Rename tick speed dictionary and lookup functions

--- utility.py
+++ utility.py
@@ -2,7 +2,7 @@ import csv
 import random
 
 
-tick_speed_lookup = {}
+tick_speed = {}
 character_initial_counter = {}
 monster_initial_counter = {}
 
@@ -14,12 +14,12 @@ with open('data/tick_speed.csv') as file:
         agi_high = int(row['agi_high'])
         tick_speed = int(row['tick_speed'])
 
-        tick_speed_lookup[agility] = tick_speed
+        tick_speed[agility] = tick_speed
 
         agility += 1
 
         while agility <= agi_high:
-            tick_speed_lookup[agility] = tick_speed
+            tick_speed[agility] = tick_speed
 
             agility += 1
 
@@ -46,11 +46,11 @@ with open('data/initial_counter.csv') as file:
             agility += 1
 
 
-def get_tick_speed(agility):
-    return tick_speed_lookup[agility]
+def lookup_tick_speed(agility):
+    return tick_speed[agility]
 
 
-def get_initial_counter(agility, category):
+def lookup_initial_counter(agility, category):
     if category == 'character':
         ic_low = character_initial_counter[agility][0]
         ic_high = character_initial_counter[agility][1]
