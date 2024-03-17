40fc1bf886cd4e33b8eecf1f5c5596602ddbb991
datle-dev <dle.dev@outlook.com>
2024-03-16 20:39:13 -0400

    Initial commit

---
+++ constants.py
@@ -0,0 +1,7 @@
+PATH_TICK_SPEED = 'data/tick_speed.csv'
+PATH_INITIAL_COUNTER = 'data/initial_counter.csv'
+
+PATH_MAIN_CHARACTERS = 'data/main_characters.json'
+PATH_MONSTERS = 'data/monsters.json'
+
+PATH_BLACK_MAGIC = 'data/black_magic.json'
