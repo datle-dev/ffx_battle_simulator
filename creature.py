5a5dc6f5d9c4d8ad8b5be7569dff77709bf1f979
datle-dev <dle.dev@outlook.com>
2024-03-16 20:15:19 -0400

    Make Creature attributes based on enums
    -

--- creature.py
+++ creature.py
@@ -1,11 +1,13 @@
+from typing import Union
 from dataclasses import dataclass, field
 import json
 
+from enums import Character, Monster, CreatureType
 
 @dataclass
 class Creature:
-    name: str
-    category: str
+    _name: Union[Character, Monster]
+    category: CreatureType
     level: int
     hp: int
     hp_max: int
@@ -23,6 +25,16 @@ class Creature:
     counter: int = field(default=0)
     is_ko: bool = False
 
+    # _name is StrEnum, so set use @property and @setter for convenience
+    # when printing the name attribute
+    @property
+    def name(self) -> str:
+        return self._name.value
+    
+    @name.setter
+    def name(self, value_as_enum: Character | Monster) -> None:
+        self._name = value_as_enum
+
     def apply_damage(self, damage: int) -> None:
         self.hp -= damage
 
@@ -36,26 +48,46 @@ class Creature:
         else:
             self.is_ko = False
 
-def load_creature(name, json_path) -> Creature:
+def load_creature(name: Character | Monster, json_path: str) -> Creature:
+    '''Creates Creature object from Character or Monster enum'''
     with open(json_path) as f:
         entities = json.load(f)
 
-    entity = Creature(
-        name=entities[name]['name'],
-        category=entities[name]['category'],
-        level=entities[name]['level'],
-        hp=entities[name]['hp'],
-        hp_max=entities[name]['hp_max'],
-        mp=entities[name]['mp'],
-        mp_max=entities[name]['mp_max'],
-        strength=entities[name]['strength'],
-        defense=entities[name]['defense'],
-        magic=entities[name]['magic'],
-        magic_defense=entities[name]['magic_defense'],
-        agility=entities[name]['agility'],
-        accuracy=entities[name]['accuracy'],
-        evasion=entities[name]['evasion'],
-        luck=entities[name]['luck'],
-    )
+    if entities[name]['category'] == CreatureType.CHARACTER:
+        entity = Creature(
+            _name=Character(entities[name]['name']),
+            category=CreatureType(entities[name]['category']),
+            level=entities[name]['level'],
+            hp=entities[name]['hp'],
+            hp_max=entities[name]['hp_max'],
+            mp=entities[name]['mp'],
+            mp_max=entities[name]['mp_max'],
+            strength=entities[name]['strength'],
+            defense=entities[name]['defense'],
+            magic=entities[name]['magic'],
+            magic_defense=entities[name]['magic_defense'],
+            agility=entities[name]['agility'],
+            accuracy=entities[name]['accuracy'],
+            evasion=entities[name]['evasion'],
+            luck=entities[name]['luck'],
+        )
+    elif entities[name]['category'] == CreatureType.MONSTER:
+        entity = Creature(
+            _name=Monster(entities[name]['name']),
+            category=CreatureType(entities[name]['category']),
+            level=entities[name]['level'],
+            hp=entities[name]['hp'],
+            hp_max=entities[name]['hp_max'],
+            mp=entities[name]['mp'],
+            mp_max=entities[name]['mp_max'],
+            strength=entities[name]['strength'],
+            defense=entities[name]['defense'],
+            magic=entities[name]['magic'],
+            magic_defense=entities[name]['magic_defense'],
+            agility=entities[name]['agility'],
+            accuracy=entities[name]['accuracy'],
+            evasion=entities[name]['evasion'],
+            luck=entities[name]['luck'],
+        )
 
     return entity
